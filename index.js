ea237493016eb705b0f595ac735b4d7ee09fd3d1
jake-knox- <jakeknox11@hotmail.co.uk>
2023-05-02 18:55:07 +0100

    adding files

---
+++ index.js
@@ -0,0 +1,10 @@
+
+console.log("test")
+
+
+// add cards to list of elements
+// add button in middle
+
+// add on click -> generates random letternumber combos to make hex
+// st bg color to hex code generated eg -> #c7ff34 (lime green)
+// click button again to rand the colours again
